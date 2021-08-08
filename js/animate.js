99e9174d29718b33e19b8dfef7d305ea170b367c
zhaoze-jpg <76829546+zhaoze-jpg@users.noreply.github.com>
2021-08-08 11:21:56 +0800

    Update animate.js

--- js/animate.js
+++ js/animate.js
@@ -5,7 +5,7 @@ function animate(element, target, callback) {
         // ❤ 问题1：step为小数时，位数的问题可能会导致移动的步长不准确。
         // 解决：转化为整数处理,且必须向上取整
         var step = (target - element.offsetLeft) / 10
-        console.log(`target:${target} offsetLeft:${element.offsetLeft}  step:${step}`);
+
         // 若移动方向为正，则step>0,应该向上取整；若移动方向为负，则step<0,应该向下取整
         step = step > 0 ? Math.ceil(step) : Math.floor(step);
         // 注意： 这里为了使元素能够原路返回， 必须使用 === ，而不能使用 >=
@@ -15,6 +15,6 @@ function animate(element, target, callback) {
             callback && callback()
         }
         element.style.left = element.offsetLeft + step + 'px'
-        console.log(element.style.left);
+
     }, 15)
 }
